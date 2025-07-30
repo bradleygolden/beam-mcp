464037c8973185b23ad8f5312d45db47728f9c8a
Bradley Golden <golden.bradley@gmail.com>
2025-07-30 13:53:58 -0500

    First commit 🚀

---
+++ lib/beam_mcp.ex
@@ -0,0 +1,18 @@
+defmodule BeamMCP do
+  @moduledoc """
+  Documentation for `BeamMCP`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BeamMCP.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/beam_mcp_test.exs
@@ -0,0 +1,8 @@
+defmodule BeamMCPTest do
+  use ExUnit.Case
+  doctest BeamMCP
+
+  test "greets the world" do
+    assert BeamMCP.hello() == :world
+  end
+end
